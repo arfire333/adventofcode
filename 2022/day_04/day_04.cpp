66331fc433297c65632f37b001f1663de7a75443
Ar Fire <72059150+arfire333@users.noreply.github.com>
2022-12-04 00:39:51 -0500

    Day 4
      Complete - Slow to capture the input and wondering if there is
      a better way.

--- 2022/day_04/day_04.cpp
+++ 2022/day_04/day_04.cpp
@@ -6,22 +6,51 @@
 
 using namespace std;
 
-int problem_a(const vector<int> in){
-
-  return 0;
+bool contained( vector<int> l, vector<int> r){  
+  if( l[0] <= r[0] && l[1] >=r[1] ){    
+    return true;
+  }else
+  if( r[0] <= l[0] && r[1] >=l[1] ){
+    return true;
+  }
+  return false;
 }
 
-int problem_b(const vector<int> in){
+bool overlap(vector<int>l, vector<int> r){
+  if( l[0] <= r[1] && l[1] >=r[0] ){        
+    return true;
+  }else  if( r[0] <= l[1] && r[1] >=l[0] ){    
+    return true;
+  }
+  return false;
+}
 
-  return 0;
+int problem_a(const vector<vector<int>> in){
+  int count=0;
+  for(int  i=0; i< in.size(); i+=2){
+    count += (contained(in[i],in[i+1])? 1: 0 ) ;
+  }
+  return count;
+}
+
+int problem_b(const vector<vector<int>> in){
+  int count=0;
+  for(int  i=0; i< in.size(); i+=2){
+    count += (overlap(in[i],in[i+1])? 1: 0 ) ;
+  }
+  return count;  
 }
 
 int main(int argc, char** argv){
-  vector<int> data;
-  // Get data
-  int i=0;
-  while( cin >> i ){
-    data.push_back(i);
+  vector<vector<int>> data;
+  // Get data 
+  string s;
+  int i,j,k,l=0;
+  while( cin >> s ){
+    sscanf( s.c_str(),"%d-%d,%d-%d",&i,&j,&k,&l );
+    cout << i << " " << j << " " << k << " " << l << "\n";
+    data.push_back({i,j});
+    data.push_back({k,l});
   }
 
   cout << "Part 1: " << problem_a(data) << "\n";
