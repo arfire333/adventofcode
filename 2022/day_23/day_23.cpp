377bbd0a458a738cf67f9569ebb041b6c0f9c47c
Ar Fire <72059150+arfire333@users.noreply.github.com>
2022-12-15 23:45:17 -0500

    Pre Day 16 - Updated tempalte

--- 2022/day_23/day_23.cpp
+++ 2022/day_23/day_23.cpp
@@ -6,24 +6,25 @@
 
 using namespace std;
 
-int problem_a(const vector<int> in){
+int problem_a(){
 
   return 0;
 }
 
-int problem_b(const vector<int> in){
+int problem_b(){
 
   return 0;
 }
 
 int main(int argc, char** argv){
-  vector<int> data;
-  // Get data
-  int i=0;
-  while( cin >> i ){
-    data.push_back(i);
+    
+  string s;
+  while( getline(cin ,s)){
+    
   }
+  int ans1=problem_a();
+  int ans2=problem_b();
 
-  cout << "Part 1: " << problem_a(data) << "\n";
-  cout << "Part 2: " << problem_b(data) << "\n";
+  cout << "Part 1: " << problem_a() << "\n";
+  cout << "Part 2: " << problem_b() << "\n";
 }
